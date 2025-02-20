6436beb8a648d5cf4e72b109c397d0dc1c827899
edith.githinji <edithgithinji2020@gmail.com>
2025-02-20 16:38:47 +0200

    User and student and teacher

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,16 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name, knowledge = []):
+       super().__init__(first_name, last_name)
+       self.knowledge = knowledge
     
-    def learn(self):
-        pass
+    def learn(self, new_knowledge):
+        return  self.knowledge.append(new_knowledge)
+
+
+Edith = Student("Edith", "Mackena")
+print(Edith.learn("English"))
+print(Edith.learn("Java"))
+print(Edith.knowledge)
